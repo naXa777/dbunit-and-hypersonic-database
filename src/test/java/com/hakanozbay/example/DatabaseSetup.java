d6fe51397b0e748aa8a207ec6eb49c6aa73a12ab
Hakan Ozbay <hakan.ozbay@gmail.com>
2016-09-01 23:16:22 +0100

    - Create sample test database and setup class

---
+++ src/test/java/com/hakanozbay/example/DatabaseSetup.java
@@ -0,0 +1,88 @@
+package com.hakanozbay.example;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.database.QueryDataSet;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatDtdDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+
+public class DatabaseSetup {
+
+	static IDatabaseTester databaseTester;
+
+	public static void databaseSetup() throws Exception
+	{
+		if (databaseTester == null)
+			databaseConnectionCreation();
+		
+		databaseTester.getConnection().getConnection().prepareStatement("TRUNCATE SCHEMA PUBLIC AND COMMIT").execute();
+		DatabaseOperation.CLEAN_INSERT.execute(databaseTester.getConnection(), getDataSet());
+	}
+
+	public static void databaseConnectionCreation() throws Exception
+	{
+		databaseTester = new JdbcDatabaseTester
+				("org.hsqldb.jdbcDriver",
+						"jdbc:hsqldb:file:src/test/resources/test_database;ifexists=false;shutdown=true",
+						"SA",
+						"",
+						"PUBLIC");
+		
+	}
+
+	private static IDataSet getDataSet() throws Exception {
+		if (!new File("src/test/resources/dataset.xml").exists())
+			getData();
+
+		if (!new File("src/test/resources/test_database.dtd").exists())
+			getDTD();
+
+		return new FlatXmlDataSetBuilder().setDtdMetadata(true)
+				.setMetaDataSetFromDtd(new FileInputStream("src/test/resources/test_database.dtd")).build(new FileInputStream("src/test/resources/dataset.xml"));
+	}
+
+
+	private static void getDTD() throws FileNotFoundException, IOException, SQLException, ClassNotFoundException, DatabaseUnitException
+	{
+		// database connection
+		Connection jdbcConnection = DriverManager.getConnection(
+				"server",
+				"username", 
+				"password");
+		IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+
+		// write DTD file
+		FlatDtdDataSet.write(connection.createDataSet(), new FileOutputStream("src/test/resources/test_database.dtd"));
+	}
+
+
+	private static void getData() throws FileNotFoundException, IOException, SQLException, ClassNotFoundException, DatabaseUnitException
+	{
+		Connection jdbcConnection = DriverManager.getConnection(
+				"server",
+				"username",
+				"password");
+		IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+
+		QueryDataSet partialDataSet = new QueryDataSet(connection);
+		partialDataSet.addTable("table_name");
+		partialDataSet.addTable("table_name");
+		FlatXmlDataSet.write(partialDataSet, new FileOutputStream("src/test/resources/dataset.xml"));
+	}
+
+}
---
+++ src/test/java/com/hakanozbay/example/DatabaseSetupTest.java
@@ -0,0 +1,13 @@
+package com.hakanozbay.example;
+
+import org.junit.Test;
+
+public class DatabaseSetupTest {
+
+	@Test
+	public void test() throws Exception 
+	{
+		DatabaseSetup.databaseSetup();
+	}
+
+}
